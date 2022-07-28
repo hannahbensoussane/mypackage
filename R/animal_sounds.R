30403b6f5feae9bbecdbfadfd112c4d32f6b76e2
Hannah Bensoussane <hannahben123@hotmail.co.uk>
2022-07-28 11:41:31 +0100

    Updated help file for animal_sounds().

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,15 @@
+#' Title
+#'
+#' @param animal A character vector of length 1 indicating the animal
+#' @param sound A character vector of length 1 indicating the sound the animal makes
+#'
+#' @return A character string giving a sentence with animal and sound inserted.
+#' @export
+#'
+#' @examples
+#' x <- "lion"
+#' y <- "roar"
+#' animal_sounds(x, y)
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
